6871d3a3381ca751df3f6e0dd2f6325624497405
MathyEM <72448225+MathyEM@users.noreply.github.com>
2021-03-30 15:42:54 +0200

    Created components for BaseInfo and ProfilePicture

---
+++ src/components/BaseInfo.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="base-info">
+        <ProfilePicture />
+        <div class="personal-info">
+            <div class="name">
+                <h5>Jane Cooper</h5>
+            </div>
+            <div class="phone">
+                <div class="flag">
+                    <img title="Sweden" class="CountryFlag" src="https://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg">
+                </div>
+                <a href="tel:4065550120">(406)555-120</a>
+            </div>
+        </div>
+        <div class="sound-controls">
+            <label for="deafen-toggle">
+                <input type="checkbox" name="deafen-toggle" id="deafen-toggle" class="control-toggle">
+                <div class="control deafen">
+                    <i class="fas fa-volume-mute"></i>
+                </div>
+            </label>
+            <label for="mute-toggle">
+                <input type="checkbox" name="mute-toggle" id="mute-toggle" class="control-toggle">
+                <div class="control mute">
+                    <i class="fas fa-microphone-slash"></i>
+                </div>
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProfilePicture from './ProfilePicture'
+
+export default {
+    name: 'BaseInfo',
+    components: {
+        ProfilePicture
+    }
+}
+</script>
+
+<style lang="scss">
+.user-container .base-info {
+    display: grid;
+    grid-template-columns: 2fr 10fr 4fr;
+    grid-gap: 0.8rem;
+
+    .personal-info {
+        margin: auto;
+
+        .phone {
+            .flag img {
+                height: 0.7rem;
+                margin-right: 0.3rem;
+            }
+        }
+    }
+
+    .sound-controls {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 0;
+        align-items: center;
+        justify-items: center;
+
+        .control {
+            $control-size: 2.2rem;
+            color: $gray;
+            background-color: $white;
+            border: $lightgray solid 1px;
+            border-radius: $border-radius;
+            height: $control-size;
+            width: $control-size;
+            display: flex;
+            cursor: pointer;
+
+            svg {
+                margin: auto;
+            }
+
+        }
+        
+        .control-toggle {
+            visibility: hidden;
+            position: absolute;
+
+            &:checked ~ .control {
+                color: $lightgreen;
+                border-color: $lightgreen;
+                background-color: transparentize($lightgreen, 0.75);
+            }
+        }
+    }
+
+}
+</style>
---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="profile-picture">
+        <div class="image">
+            <span class="user-status"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfilePicture',
+}
+</script>
+
+<style lang="scss">
+.profile-picture {
+    display: flex;
+    height: 100%;
+    width: 100%;
+
+    .image {
+        background-color: $gray;
+        border-radius: 50%;
+        width: 100%;
+        align-self: center;
+        position: relative;
+
+        &:after {
+            content: "";
+            display: block;
+            padding-bottom: 100%;
+        }
+
+        .user-status {
+            background-color: $lightgreen;
+            border: 0.15rem solid white;
+            border-radius: 50%;
+            width: 30%;
+            align-self: center;
+            position: absolute;
+            bottom: 0;
+            right: 0;
+
+            &:after {
+                content: "";
+                display: block;
+                padding-bottom: 100%;
+            }
+        }
+    }
+}
+</style>
